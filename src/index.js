d1c8a9fc98e6209ae753c2ea270274d26d0ea6fc
Jonathan Ferreira <jonathanferreira23@gmail.com>
2019-08-20 19:47:13 +0100

    Adding CRUD functions

--- src/index.js
+++ src/index.js
@@ -2,5 +2,72 @@ const express = require("express");
 
 const server = express();
 
-server.get("/projects", (req, res) => {});
+server.use(express.json());
+
+const projects = [];
+const tasks = [];
+let calls = 0;
+
+server.use((req, res, next) => {
+  calls++;
+  console.log(`Number of calls: ${calls}`);
+  return next();
+});
+
+function checkProjectExistence(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(p => p.id == id);
+  if (!project) {
+    return res.status(400).json({ error: "Project doesn't exists." });
+  }
+  return next();
+}
+
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+  projects.push(project);
+  return res.json(projects);
+});
+
+server.post("/projects/:id/tasks", checkProjectExistence, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.pop(title);
+
+  return res.json(projects);
+});
+
+server.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(projects);
+});
+
+server.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  const projectIdx = projects.findIndex(p => p.id == id);
+
+  projects.splice(projectIdx, 1);
+
+  return res.send();
+});
+
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
+
 server.listen(3000);
